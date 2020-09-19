44b71b97757c0aeda1d5eaea08d9002c40cd9520
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2020-09-19 12:35:34 +0300

    First commit

---
+++ questions.js
@@ -0,0 +1,25 @@
+data = [
+    {
+        "question": "Who wrote the Hitchhiker's Guide to the Galaxy?",
+        "answers": [
+            "Robbin William",
+            "J.J Abrams",
+            "Douglas Adams",
+            "Madonna"
+        ],
+        "correct": 3,
+        "explanation": "Douglas Adams wrote the Hitchhiker's Guide to the galaxy"
+    },
+    {
+        "question": "When did the Battle of the Bulge start?",
+        "answers": [
+            "December 1944",
+            "January 1945",
+            "November 1943",
+            "December 1945",
+            "There's no such battle"
+        ],
+        "correct": 1,
+        "explanation": "The Battle of the Bulge started on 16 December 1944 and ended in 25 January 1945"
+    }
+]
---
+++ quizScript.js
@@ -0,0 +1,51 @@
+let questions
+
+function loadQuestions() {
+    console.log(data)
+    document.getElementById("btn").className = "d-none"
+    const questionsDiv = document.getElementById("question")
+    const h4AnswerTitle = document.createElement("h4")
+    const h4QuestionTitle = document.createElement("h4")
+    h4QuestionTitle.innerHTML = "Question"
+    h4AnswerTitle.innerHTML = "Answers"
+    for (let i = 0; i < data.length; i++) {
+        const questionDiv = document.createElement("div")
+        const questionElmnt = document.createElement("h5")
+        questionElmnt.innerHTML = data[i].question
+        questionDiv.appendChild(questionElmnt)
+        const answersDiv = document.createElement("div")
+        const explanationDiv = document.createElement("div")
+        for (let x = 0; x < data[i].answers.length; x++) {
+            const answerDiv = document.createElement("div")
+            const answerElmnt = document.createElement("button")
+            answerElmnt.innerHTML = data[i].answers[x]
+            answerElmnt.className = "m-2"
+            answerElmnt.value = x + 1
+            answerDiv.appendChild(answerElmnt)
+            answersDiv.appendChild(answerDiv)
+            answerElmnt.addEventListener("click", () => {
+                while (explanationDiv.firstChild) {
+                    explanationDiv.removeChild(explanationDiv.firstChild)
+                }
+                const explanationElmnt = document.createElement("p")
+                explanationElmnt.innerHTML = data[i].explanation
+                const rightOrWrongElmnt = document.createElement("p")
+                if (parseInt(answerElmnt.value) === data[i].correct) {
+                    rightOrWrongElmnt.innerHTML = "You are correct!"
+                } else {
+                    rightOrWrongElmnt.innerHTML = "Wrong answer"
+                }
+                explanationDiv.appendChild(rightOrWrongElmnt)
+                explanationDiv.appendChild(explanationElmnt)
+            })
+        }
+        questionsDiv.appendChild(questionDiv)
+        questionsDiv.appendChild(answersDiv)
+        questionsDiv.appendChild(explanationDiv)
+    }
+}
+
+let btn = document.getElementById("btn")
+btn.addEventListener("click", () => {
+    loadQuestions()
+})
